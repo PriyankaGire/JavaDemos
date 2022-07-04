9b0ffd24acf64088008cd402fb04d6de62586aa6
PriyankaGire <priyagire94@gmail.com>
2022-07-04 14:21:13 +0530

    Added java demo application files

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args)
+	{
+		System.out.println(checkIfInputIsanEvenNumber(122));
+		
+	}
+
+	public static boolean checkIfInputIsanEvenNumber(int number)
+	{
+		return number % 2 == 0;
+	}
+}
